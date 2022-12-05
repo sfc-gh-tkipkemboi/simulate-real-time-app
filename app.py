03c8769cbf9af4292c93ddfd52496ee81c06583c
Tony Kipkemboi <117667118+sfc-gh-tkipkemboi@users.noreply.github.com>
2022-12-05 17:15:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 @st.cache
 def load_data():
+  data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+  }
+  df = pd.DataFrame(data)
   return df
 
 df = load_data()
